0f44c202ac6694ee3111101479feccf075c0260c
Ben Doyle <ben.doyle@shopify.com>
2019-04-03 14:26:43 -0400

    better splitter

--- splitter.py
+++ splitter.py
@@ -1,11 +1,7 @@
 import re
+import os
 
-file_name = '/Users/ben/src/github.com/bendoyle/warehouse_crawl/morgues/morgue-Ysoep-20190225-205833.txt'
-file_handle = open(file_name, mode='r', errors='ignore')
-contents = file_handle.read()
-file_handle.close()
-
-section_patterns = (
+SECTION_PATTERNS = (
     ('game_version',                 'Dungeon Crawl Stone Soup version 0\.23\.0 \(tiles\) character file\.'),
     ('game_seed',                    'Game seed: \d+'),
     ('prose_summary',                '\d+ [\w\s]+ \(level \d+, [\-0-9]+/\d+ HPs\)'),
@@ -28,27 +24,51 @@ section_patterns = (
     ('action_totals',                'Action\s+\|'),
 )
 
-def find_end(reduced_contents, reduced_section_patterns):
-    for section, pattern in reduced_section_patterns:
-        match = re.search(pattern, reduced_contents)
+def section_log_string(contents):
+    def find_end(reduced_contents, reduced_section_patterns):
+        for section, pattern in reduced_section_patterns:
+            match = re.search(pattern, reduced_contents)
+            if match:
+                return match.span(0)[0]
+
+        return None
+
+    sections = {}
+    for section_number, (section, pattern) in enumerate(SECTION_PATTERNS):
+        match = re.search(pattern, contents)
         if match:
-            return match.span(0)[0]
+            end_of_section_match = match.span(0)[1]
+            end_position = find_end(contents[end_of_section_match:], SECTION_PATTERNS[section_number+1:])
+            if end_position:
+                end_position = end_position + end_of_section_match
+            sections[section] = contents[0:end_position]
+            contents = contents[end_position:]
+        else:
+            sections[section] = None
 
-    return None
+    return sections
 
-sections = {}
-for section_number, (section, pattern) in enumerate(section_patterns):
-    match = re.search(pattern, contents)
-    if match:
-        end_of_section_match = match.span(0)[1]
-        import pdb; pdb.set_trace()
-        # if section == 'final_messages':
-        #     import pdb; pdb.set_trace()
-        end_position = find_end(contents[end_of_section_match:], section_patterns[section_number+1:])
-        sections[section] = contents[0:end_position]
-        contents = contents[end_position:]
-    else:
-        sections[section] = None
+if __name__ == '__main__':
+
+    # 'data/morgues/morgue-Ysoep-20190225-205833.txt'
+    file_name = os.environ.get('filename')
+    file_handle = open(file_name, mode='r', errors='ignore')
+    contents = file_handle.read()
+    file_handle.close()
+    sections = section_log_string(contents)
+
+    for section in map(lambda s: s[0], SECTION_PATTERNS):
+        try:
+            os.makedirs('data/{}'.format(section))
+        except:
+            pass
+
+        outfile_name = 'data/{}/{}-{}'.format(section, section, os.path.basename(file_name))
+        if sections[section]:
+            file_handle = open(outfile_name, 'w')
+            file_handle.write(sections[section])
+            file_handle.close()
 
 
-import pdb; pdb.set_trace()
+
+
