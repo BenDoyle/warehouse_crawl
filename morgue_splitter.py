24675f894ae45ff2a774712e7911cf2dd1709baf
Ben Doyle <ben.doyle@shopify.com>
2019-04-04 12:00:22 -0400

    refactored splitter script

--- morgue_splitter.py
+++ morgue_splitter.py
@@ -1,75 +1,93 @@
+import glob
 import re
 import os
 
-SECTION_PATTERNS = (
-    ('game_version',                 'Dungeon Crawl Stone Soup version 0\.23\.0 \(tiles\) character file\.'),
-    ('game_seed',                    'Game seed: \d+'),
-    ('prose_summary',                '\d+ [\w\s]+ \(level \d+, [\-0-9]+/\d+ HPs\)'),
-    ('final_stats',                  '\w+ the \w+ \([\w\s]+\)\s+Turns: \d+, Time: [0-9:]+'),
-    ('final_exploration_summary',    'You were on '),
-    ('final_inventory',              'Inventory:'),
-    ('final_skills',                 'Skills:'),
-    ('final_spells',                 '\n\n'),
-    # ('final_spells',                 'Your Spells\s+Type\sPower\s+Failure\s+Level\s+Hunger'),
-    ('final_library',                'Your spell library contained the following spells:'),
-    ('final_dungeon_overview',       'Dungeon Overview and Level Annotations'),
-    ('final_altars',                 'Altars:'),
-    ('final_shops',                  'Shops:'),
-    ('final_annotations',            'Annotations:'),
-    ('final_abilities',              'Innate Abilities, Weirdness & Mutations'),
-    ('final_messages',               'Message History\n\n\w+'),
-    ('final_map',                    '\n\n'),
-    ('final_vanquished',             'Vanquished Creatures'),
-    ('notes',                        'Notes'),
-    ('skill_progression',            'Skill\s+XL:'),
-    ('action_totals',                'Action\s+\|'),
-)
+SECTION_PATTERNS = {
+    'game_version':                 'Dungeon Crawl Stone Soup version 0\.23(\.\d+)?[\-\w\d]* \(\w+\) character file\.',
+    'game_seed':                    'Game seed: \d+',
+    'prose_summary':                '\d+ [\-\w\s]+ \(level \d+, [\-0-9]+/\d+',
+    'final_stats':                  '[\-\w]+\s+the\s+[\s\-\w]+\s\([\w\s]+\)\s+Turns:\s+\d+,\s+Time:\s+[0-9:]+',
+    'final_exploration_summary':    'You were [oi]n|You escaped',
+    'final_inventory':              'Inventory:',
+    'final_skills':                 'Skills:',
+    'final_spells':                 'You had [\d\w]+ spell level(s)? left.|You couldn\'t memorise any spells.',
+    'final_library':                'Your spell library contained the following spells:|Your spell library was empty.',
+    'final_dungeon_overview':       'Dungeon Overview and Level Annotations',
+    'final_altars':                 'Altars:',
+    'final_shops':                  'Shops:',
+    'final_annotations':            'Annotations:',
+    'final_abilities':              'Innate Abilities, Weirdness & Mutations',
+    'final_messages':               'Message History\n\n',
+    'final_vanquished':             'Vanquished Creatures',
+    'notes':                        'Notes',
+    'skill_progression':            'Skill\s+XL:',
+    'action_totals':                'Action\s+\|',
+}
+
+OPTIONAL_SECTIONS = [
+    'final_altars',      # some logs do not print an altars list, even if found. e.g. morgue-stickyfingers-20190213-154231.txt
+    'final_abilities',   # games without mutations
+    'final_shops',       # shops are not discovered in this game
+    'final_annotations', # annotatable things are not present in this game
+    'final_vanquished',  # games with no vanquished creatures
+    'skill_progression', # games that end before level 2 is reached
+    'action_totals',     # games without a single action
+]
 
 def section_log_string(contents):
-    def find_end(reduced_contents, reduced_section_patterns):
-        for section, pattern in reduced_section_patterns:
-            match = re.search(pattern, reduced_contents)
-            if match:
-                return match.span(0)[0]
 
-        return None
-
-    sections = {}
-    for section_number, (section, pattern) in enumerate(SECTION_PATTERNS):
-        match = re.search(pattern, contents)
-        if match:
-            end_of_section_match = match.span(0)[1]
-            end_position = find_end(contents[end_of_section_match:], SECTION_PATTERNS[section_number+1:])
-            if end_position:
-                end_position = end_position + end_of_section_match
-            sections[section] = contents[0:end_position]
-            contents = contents[end_position:]
+    def min_or_none(breakpoints, start):
+        larger = set(filter(lambda x: x > start, breakpoints))
+        if larger:
+            return min(larger)
         else:
-            sections[section] = None
+            return None
+
+    section_matches = {
+        section: list(re.finditer(pattern, contents))
+        for section, pattern in SECTION_PATTERNS.items()
+    }
+
+    breakpoints = {
+        match.span(0)[0]
+        for section, matches in section_matches.items()
+        for match in matches
+    } - {None}
+
+    section_spans = {
+        section: matches[0].span(0) if len(matches) > 0 else None
+        for section, matches in section_matches.items()
+    }
+
+    sections = {
+        section: contents[span[0]:min_or_none(breakpoints, span[1])] if span else None
+        for section, span in section_spans.items()
+    }
 
     return sections
 
 if __name__ == '__main__':
 
-    # 'data/morgues/morgue-Ysoep-20190225-205833.txt'
-    file_name = os.environ.get('filename')
-    file_handle = open(file_name, mode='r', errors='ignore')
-    contents = file_handle.read()
-    file_handle.close()
-    sections = section_log_string(contents)
+    files = glob.glob('{}/*.txt'.format(os.environ.get('path')))
 
-    for section in map(lambda s: s[0], SECTION_PATTERNS):
-        try:
-            os.makedirs('data/0.23.0/{}'.format(section))
-        except:
-            pass
+    for file_name in files:
+        print(file_name)
+        file_handle = open(file_name, mode='r', errors='ignore')
+        contents = file_handle.read()
+        file_handle.close()
+        sections = section_log_string(contents)
 
-        outfile_name = 'data/0.23.0/{}/{}-{}'.format(section, section, os.path.basename(file_name))
-        if sections[section]:
-            file_handle = open(outfile_name, 'w')
-            file_handle.write(sections[section])
-            file_handle.close()
+        for section in SECTION_PATTERNS.keys():
+            try:
+                os.makedirs('data/0.23/{}'.format(section))
+            except:
+                pass
 
+            outfile_name = 'data/0.23/{}/{}-{}'.format(section, section, os.path.basename(file_name))
 
-
-
+            if sections[section]:
+                file_handle = open(outfile_name, 'w')
+                file_handle.write(sections[section])
+                file_handle.close()
+            elif section not in OPTIONAL_SECTIONS:
+                print("  missing {}".format(section))
