a6c703318a8baa5276a8e9c310e99d5d77c1713f
Ben Doyle <ben.doyle@shopify.com>
2019-04-04 15:25:47 -0400

    Refactor altars parser

--- parsers/final_altars_parser.py
+++ parsers/final_altars_parser.py
@@ -1,29 +1,21 @@
-import re
+from common import get_game_id
+from common import write_rows_to_csv
+from common import read_text_file
 import os
-import csv
+import glob
 
-
-def get_game_id(file_name):
-    basename = os.path.basename(file_name)
-    match = re.match('(.*)-morgue-(.*)-(\d{8})-(\d{6})\.txt', basename)
-
-    return '{}-{}-{}'.format(match.group(2), match.group(3), match.group(4))
-
-if __name__ == '__main__':
-
-    file_name = os.environ.get('filename')
-    file_handle = open(file_name, mode='r', errors='ignore')
-    contents = file_handle.read()
-    file_handle.close()
-    game_id = get_game_id(file_name)
-    rows = [
+def get_rows(contents):
+    return [
         [game_id, altar]
         for altar in contents.strip().split('\n')[1:]
     ]
-    if rows:
-        outfile_name = file_name[0:-3] + 'csv'
-        with open(outfile_name, 'w') as f:
-            writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            writer.writerows(rows)
 
+if __name__ == '__main__':
+    files = glob.glob('{}/*.txt'.format(os.environ.get('path')))
+    for file_name in files:
+        contents = read_text_file(file_name)
+        game_id = get_game_id(file_name)
+        rows = get_rows(contents)
+        write_rows_to_csv(rows, file_name)
+        print(file_name)
 
