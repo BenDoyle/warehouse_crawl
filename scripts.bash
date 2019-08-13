0210b52e7c8f036a63f6bede6090dc247abc68e1
Ben Doyle <ben.doyle@shopify.com>
2019-08-13 15:58:38 -0600

    update scripts

--- scripts.bash
+++ scripts.bash
@@ -1,3 +1,4 @@
+rm -rf data/0.23
 MORGUES=data/morgues/ OUTPUT=data/ python processors/morgue_splitter/morgue_splitter-0.23.py;
 path=data/0.23/notes/ python parsers/altars.py;
 path=data/0.23/notes/ python parsers/bought.py;
@@ -25,32 +26,29 @@ path=data/0.23/final_vanquished/ python parsers/final_vanquished_parser.py;
 path=data/0.23/game_seed/ python parsers/game_seed_parser.py;
 path=data/0.23/game_version/ python parsers/game_version_parser.py;
 
-cat data/0.23/notes/altars/*.csv > data/0.23/altars.csv;
-cat data/0.23/notes/bought/*.csv > data/0.23/bought.csv;
-cat data/0.23/notes/fell/*.csv > data/0.23/fell.csv;
-cat data/0.23/notes/found/*.csv > data/0.23/found.csv;
-cat data/0.23/notes/identified/*.csv > data/0.23/identified.csv;
-cat data/0.23/notes/killed/*.csv > data/0.23/killed.csv;
-cat data/0.23/notes/learned/*.csv > data/0.23/learned.csv;
-cat data/0.23/notes/mutation/*.csv > data/0.23/mutation.csv;
-cat data/0.23/notes/noticed/*.csv > data/0.23/noticed.csv;
-cat data/0.23/notes/orb/*.csv > data/0.23/orb.csv;
-cat data/0.23/notes/shaft/*.csv > data/0.23/shaft.csv;
-cat data/0.23/notes/skill_progression/*.csv > data/0.23/skill_progression.csv;
-cat data/0.23/notes/unlearned/*.csv > data/0.23/unlearned.csv;
-cat data/0.23/notes/upgrades/*.csv > data/0.23/upgrades.csv;
-cat data/0.23/notes/win/*.csv > data/0.23/win.csv;
-cat data/0.23/notes/worship/*.csv > data/0.23/worship.csv;
-cat data/0.23/notes/xp_progression/*.csv > data/0.23/xp_progression.csv;
-
-cat data/0.23/action_totals/*.csv > data/0.23/action_totals.py;
-cat data/0.23/final_abilities/*.csv > data/0.23/final_abilities_parser.py;
-cat data/0.23/final_altars/*.csv > data/0.23/final_altars_parser.py;
-cat data/0.23/final_skills/*.csv > data/0.23/final_skills_parser.py;
-cat data/0.23/final_stats/*.csv > data/0.23/final_stats_parser.py;
-cat data/0.23/final_vanquished/*.csv > data/0.23/final_vanquished_parser.py;
-cat data/0.23/game_seed/*.csv > data/0.23/game_seed_parser.py;
-cat data/0.23/game_version/*.csv > data/0.23/game_version_parser.py;
-
-
-
+mkdir data/0.23/summaries;
+cat data/0.23/notes/altars/*.csv > data/0.23/summaries/altars.csv;
+cat data/0.23/notes/bought/*.csv > data/0.23/summaries/bought.csv;
+cat data/0.23/notes/fell/*.csv > data/0.23/summaries/fell.csv;
+cat data/0.23/notes/found/*.csv > data/0.23/summaries/found.csv;
+cat data/0.23/notes/identified/*.csv > data/0.23/summaries/identified.csv;
+cat data/0.23/notes/killed/*.csv > data/0.23/summaries/killed.csv;
+cat data/0.23/notes/learned/*.csv > data/0.23/summaries/learned.csv;
+cat data/0.23/notes/mutation/*.csv > data/0.23/summaries/mutation.csv;
+cat data/0.23/notes/noticed/*.csv > data/0.23/summaries/noticed.csv;
+cat data/0.23/notes/orb/*.csv > data/0.23/summaries/orb.csv;
+cat data/0.23/notes/shaft/*.csv > data/0.23/summaries/shaft.csv;
+cat data/0.23/notes/skill_progression/*.csv > data/0.23/summaries/skill_progression.csv;
+cat data/0.23/notes/unlearned/*.csv > data/0.23/summaries/unlearned.csv;
+cat data/0.23/notes/upgrades/*.csv > data/0.23/summaries/upgrades.csv;
+cat data/0.23/notes/win/*.csv > data/0.23/summaries/win.csv;
+cat data/0.23/notes/worship/*.csv > data/0.23/summaries/worship.csv;
+cat data/0.23/notes/xp_progression/*.csv > data/0.23/summaries/xp_progression.csv;
+cat data/0.23/action_totals/*.csv > data/0.23/summaries/action_totals.csv;
+cat data/0.23/final_abilities/*.csv > data/0.23/summaries/final_abilities.csv;
+cat data/0.23/final_altars/*.csv > data/0.23/summaries/final_altars.csv;
+cat data/0.23/final_skills/*.csv > data/0.23/summaries/final_skills.csv;
+cat data/0.23/final_stats/*.csv > data/0.23/summaries/final_stats.csv;
+cat data/0.23/final_vanquished/*.csv > data/0.23/summaries/final_vanquished.csv;
+cat data/0.23/game_seed/*.csv > data/0.23/summaries/game_seed.csv;
+cat data/0.23/game_version/*.csv > data/0.23/summaries/game_version.csv;
